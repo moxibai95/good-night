e25fae7c2a2e93bd516a5bc6f477e9e394a39ec8
刘洋 <liuy58@ziroom.com>
2018-11-21 16:48:27 +0800

    更改springboot启动，项目启动成功

--- src/main/java/com/goodnight/background/Application.java
+++ src/main/java/com/goodnight/background/Application.java
@@ -1,7 +1,9 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+package com.goodnight.background;
 
-@SpringBootConfiguration
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
